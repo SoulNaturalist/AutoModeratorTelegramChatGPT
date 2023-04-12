b60564f8f969b2a267c404dc313086ab4cb52441
SoulNaturalist <58743402+SoulNaturalist@users.noreply.github.com>
2023-04-12 06:46:50 +0300

    Create main.py

---
+++ examples/main.py
@@ -0,0 +1,15 @@
+"""
+Main easiyer way to automoderation
+"""
+
+class AutoModeration:
+  def __init__(self, openai_token: str, ban_words: list, delete: bool, ban: bool) -> None:
+    self.openai_token = openai_token
+    self.ban_words = ban_words
+    self.delete = delete
+    self.ban = ban
+  def gen_context_msg_gpt() -> None:
+    if self.ban_words:
+      return """Hi, read this message\n{0} and if it contains at least one word of their list - {1}\nAlso, do you think this message is spam?, say yes or no"""
+    else:
+      return """Determine whether this message is spam or not, if yes, write yes in the answer"""
